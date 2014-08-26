459723ccf10c87d7f726633116f676339f69ed08
Yuwei Ba <xiaobayuwei@gmail.com>
2014-08-26 20:42:17 +0800

    Initial Commit

---
+++ FuckVoca/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FuckVoca
+//
+//  Created by Yuwei Ba on 8/26/14.
+//  Copyright (c) 2014 ___YUWEIBA___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YWAppDelegate class]));
+    }
+}
