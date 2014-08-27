da7b318a87a5046217ec2af7355b6cc602e1cf2c
Yuwei Ba <xiaobayuwei@gmail.com>
2014-08-27 19:26:44 +0800

    Update

--- FuckVoca/YWToDoItem.h
+++ FuckVoca/YWToDoItem.h
@@ -8,10 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
-@interface YWToDoItem : NSObject
+@interface YWToDoItem : NSObject {
+    int _uid;
+    NSString *_itemName;
+    BOOL _completed;
+    NSDate *_creationDate;
+}
 
-@property NSString *itemName;
-@property BOOL completed;
-@property (readonly) NSDate *creationDate;
+@property (nonatomic, assign) int uid;
+@property (nonatomic, copy) NSString *itemName;
+@property (nonatomic)BOOL completed;
+@property (nonatomic, readonly) NSDate *creationDate;
+
+- (id) initWithUID: (int) uid name:(NSString *) itemName completed: (BOOL) completed
+      creationTime: (NSData *) creationDate;
 
 @end
--- FuckVoca/YWToDoItem.m
+++ FuckVoca/YWToDoItem.m
@@ -10,4 +10,20 @@
 
 @implementation YWToDoItem
 
+@synthesize uid = _uid;
+@synthesize itemName = _itemName;
+@synthesize completed = _completed;
+@synthesize creationDate = _creationDate;
+
+- (id) initWithUID:(int)uid name:(NSString *)itemName completed:(BOOL)completed creationTime:(NSData *)creationDate
+{
+    if ((self = [super init])) {
+        self.uid = uid;
+        self.itemName = itemName;
+        self.completed = completed;
+    }
+    
+    return self;
+}
+
 @end
